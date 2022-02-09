0c951b969c1a5cf2e6016f3b145d7e72cb9dc191
Tommy Karjamenawi <54915266+tommykarjamenawi@users.noreply.github.com>
2022-02-09 12:54:24 +0100

    added lecture 1/2 base assignment

---
+++ vue-lecture1-master/vue-lecture1-master/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <StockList />
+  </div>
+</template>
+
+<script>
+import StockList from "./components/StockList.vue";
+
+export default {
+  name: "App",
+  components: {
+    StockList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
